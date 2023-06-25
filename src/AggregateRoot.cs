f040881999bdf1ea360f52b1710242c98d6d44e4
Artem Kunich <artem13081991@gmail.com>
2023-06-25 11:25:28 +0200

    Add RaiseDomainEvent method to aggregate root

--- src/AggregateRoot.cs
+++ src/AggregateRoot.cs
@@ -1,5 +1,19 @@
+using System.Collections.Generic;
+
 namespace Akunich.Domain.Abstractions;
 
 public class AggregateRoot<TKey> : Entity<TKey>
 {
+    private readonly List<IDomainEvent> _domainEvents;
+
+    protected AggregateRoot()
+    {
+        _domainEvents = new List<IDomainEvent>();
+    }
+    
+    public void RaiseDomainEvent(IDomainEvent @event) => _domainEvents.Add(@event);
+
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    public void ClearDomainEvents() => _domainEvents.Clear();
 }
