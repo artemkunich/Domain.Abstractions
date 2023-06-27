8001bce5109ed8158b31a8250f7f322c254c4f3b
Artem Kunich <artem13081991@gmail.com>
2023-06-27 21:41:57 +0200

    Add CancellationToken to IReadOnlyRepository

--- src/IReadOnlyRepository.cs
+++ src/IReadOnlyRepository.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Akunich.Domain.Abstractions;
 
 public interface IReadOnlyRepository<TEntity, in TKey>
 {
-    Task<TEntity> GetByIdAsync(TKey id);
+    Task<TEntity> GetByIdAsync(TKey id, CancellationToken cancellation);
 
-    Task<IEnumerable<TEntity>> GetByExpressionAsync(Expression<Func<TEntity, bool>> expression);
+    Task<IEnumerable<TEntity>> GetByExpressionAsync(Expression<Func<TEntity, bool>> expression, CancellationToken cancellation);
 }
